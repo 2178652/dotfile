11b643847b375c17cc43ebd425c3a7d76c6bc0b1
charles garand <2178652@cstjean.qc.ca>
2024-09-08 20:17:32 -0400

    1.01

--- Install.sh
+++ Install.sh
@@ -9,9 +9,9 @@ mkdir projets
 
 sudo pacman -Sy git neovim gcc gdb python3 python-pip python-virtualenv
 
-SOURCE_DIR="$HOMe/dotfile"
+SOURCE_DIR="$HOME/dotfile"
 DEST_DIR="$HOME"
 
-cp "SOURCE_DIR/.bashrc" "DEST_DIR/.bashrc"
-cp "SOURCE_DIR/.gitconfig" "DEST_DIR/.gitconfig"
+cp "$SOURCE_DIR/.bashrc" "$DEST_DIR/.bashrc"
+cp "$SOURCE_DIR/.gitconfig" "$DEST_DIR/.gitconfig"
 
